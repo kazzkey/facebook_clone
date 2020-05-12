f5be5931d9d92aec5b9a9b2d2e9d9bc977cf5e11
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-05-12 14:07:57 +0900

    pictureとuserを関連付けて、投稿システムを実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,44 +16,33 @@ class PicturesController < ApplicationController
     end
   end
   def confirm
-    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
   end
 
   def edit
   end
 
   def create
-    @picture = Picture.new(picture_params)
-
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.save
+      redirect_to pictures_path, notice: 'アップロード完了！'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
-      else
-        format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: '編集完了！'
+    else
+      render :edit
     end
   end
 
   def destroy
     @picture.destroy
-    respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pictures_url, notice: '削除完了！'
   end
 
   private
